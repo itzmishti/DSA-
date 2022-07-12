ac7274394a660744c8732fd06db489c3f7645bcf
itzmishti <vgupta4_be20@thapar.edu>
2022-07-12 09:15:28 +0530

    q4

---
+++ LeetCode/day1 q4.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum=INT_MIN;
+        int curr=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            curr=curr+nums[i];
+            maxSum=max(curr,maxSum);
+            if(0>curr)
+                curr=0;
+        }
+        return maxSum;
+    }
+};
