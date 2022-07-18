332007c4f0f9f847782510f5317e239f976cfbc6
itzmishti <vgupta4_be20@thapar.edu>
2022-07-18 07:04:38 +0530

    joeys string

---
+++ Random/joey.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int count(string s,char c)
+{
+    int count=0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] ==  c)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int sumOfString(string s)
+{
+    int zero,one,two,three,four,five,six,seven,eight,nine;
+    zero=count(s,'z');
+    two=count(s,'w');
+    four=count(s,'u');
+    five=count(s,'f')-four;
+    six=count(s,'x');
+    seven=count(s,'s')-six;
+    eight=count(s,'g');
+    nine=count(s,'i')-five-six-eight;
+    one=count(s,'o')-zero-two-four;
+    three=count(s,'r')-zero-four;
+    
+    int sum=one+2*two+3*three+4*four+5*five+6*six+7*seven+8*eight+9*nine;
+    return sum;
+    
+}
+int main()
+{
+    string s;
+    cin>>s;
+    int ans=sumOfString(s);
+    cout<<ans;
+    return 0;
+}
